3a41588571d432abdb78479473d0a79ddfe68243
StefanRu <StefanRu@users.noreply.github.com>
2018-12-22 23:50:54 +0100

    a

--- bsl.py
+++ bsl.py
@@ -40,12 +40,12 @@ timeregex = re.compile('^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$')
 departuresListRaw = filter(None, departuresListRaw)
 
 for row in departuresListRaw:
-    print(row[1])
+    print(row[0])
 #    if re.search(timeregex, row[1]):
 #        departuresList.append()
 
-#for s in departuresList:
-#    print(*s)
+for s in departuresListRaw:
+    print(*s)
 
 
 #if cell.text[:2].isnumeric() = True
