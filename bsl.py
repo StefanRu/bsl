50e19b219bd21e6edbe3a23b297f7e86daec6b06
StefanRu <StefanRu@users.noreply.github.com>
2018-12-22 23:48:14 +0100

    a

--- bsl.py
+++ bsl.py
@@ -37,7 +37,10 @@ for tr in table_rows:
 
 timeregex = re.compile('^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$')
 
+departuresListRaw = filter(None, departuresListRaw)
+
 for row in departuresListRaw:
+    if
     print(row[1])
 #    if re.search(timeregex, row[1]):
 #        departuresList.append()
